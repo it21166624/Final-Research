38f7a1851616d8eba101c062ba94a0268d2d4a78
it21176906 <100943027+it21176906@users.noreply.github.com>
2025-03-17 21:19:05 +0530

    Back end

---
+++ Egg_Pedict_BackEnd/Egg_Pedict_BackEnd/Migrations/20250315130852_addnewStressDataTable.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Egg_Pedict_BackEnd.Migrations
+{
+    /// <inheritdoc />
+    public partial class addnewStressDataTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StressData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Temperature = table.Column<double>(type: "double precision", nullable: false),
+                    Humidity = table.Column<double>(type: "double precision", nullable: false),
+                    FeedIntakePerHen = table.Column<double>(type: "double precision", nullable: false),
+                    WaterIntakePerHen = table.Column<double>(type: "double precision", nullable: false),
+                    AirQuality = table.Column<string>(type: "text", nullable: false),
+                    Lighting = table.Column<double>(type: "double precision", nullable: false),
+                    CageDensity = table.Column<double>(type: "double precision", nullable: false),
+                    Vocalization = table.Column<double>(type: "double precision", nullable: false),
+                    BodyTemperature = table.Column<double>(type: "double precision", nullable: false),
+                    Heartbeat = table.Column<int>(type: "integer", nullable: false),
+                    StressLevel = table.Column<int>(type: "integer", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StressData", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StressData");
+        }
+    }
+}
---
+++ Egg_Pedict_BackEnd/Egg_Pedict_BackEnd/Model/SensorData.cs
@@ -0,0 +1,16 @@
+﻿namespace Egg_Pedict_BackEnd.Model
+{
+    public class SensorData
+    {
+        public int Id { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public double Light_Hours { get; set; }
+        public double Hen_Age_weeks { get; set; }
+        public double Feed_Quantity { get; set; }
+        public double Hen_Count { get; set; }
+        public double Egg_count { get; set; }
+        public string Health_Status { get; set; } = string.Empty; 
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
